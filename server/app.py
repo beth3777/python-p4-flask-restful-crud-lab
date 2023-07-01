fc76184e1cb0d84e5c78fd7264d29078fe733f17
email <bethkimaku@gmail.com>
2023-07-01 06:45:08 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -44,6 +44,27 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        data = request.get_json()
+        plant.is_in_stock = data.get("is_in_stock", plant.is_in_stock)
+        
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 204)
+
+
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
